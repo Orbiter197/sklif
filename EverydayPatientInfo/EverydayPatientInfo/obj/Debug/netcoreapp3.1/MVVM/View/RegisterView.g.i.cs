7748b97af4828dc53d248ffbe803fe6533149512
Svyatoslav Shmarin <63782406+Orbiter197@users.noreply.github.com>
2021-09-20 19:10:02 +0300

    MarkDown update

--- EverydayPatientInfo/EverydayPatientInfo/obj/Debug/netcoreapp3.1/MVVM/View/RegisterView.g.i.cs
+++ EverydayPatientInfo/EverydayPatientInfo/obj/Debug/netcoreapp3.1/MVVM/View/RegisterView.g.i.cs
@@ -53,7 +53,7 @@ namespace EverydayPatientInfo.MVVM.View {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/EverydayPatientInfo;component/mvvm/view/registerview.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/EverydayPatientInfo;V1.0.0.0;component/mvvm/view/registerview.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\..\MVVM\View\RegisterView.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
--- EverydayPatientInfo/EverydayPatientInfo/obj/Release/netcoreapp3.1/MVVM/View/MainContentView.g.cs
+++ EverydayPatientInfo/EverydayPatientInfo/obj/Release/netcoreapp3.1/MVVM/View/MainContentView.g.cs
@@ -53,7 +53,7 @@ namespace EverydayPatientInfo.MVVM.View {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/EverydayPatientInfo;V1.0.0.0;component/mvvm/view/maincontentview.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/EverydayPatientInfo;component/mvvm/view/maincontentview.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\..\MVVM\View\MainContentView.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
