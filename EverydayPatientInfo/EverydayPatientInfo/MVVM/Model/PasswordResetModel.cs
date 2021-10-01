5e1d8e61ebf49a76e3f74c13c5730e70f007e841
Svyatoslav Shmarin <63782406+Orbiter197@users.noreply.github.com>
2021-10-01 10:43:56 +0300

    Logic for resetting password added

---
+++ EverydayPatientInfo/EverydayPatientInfo/MVVM/Model/PasswordResetModel.cs
@@ -0,0 +1,22 @@
+﻿using EverydayPatientInfo.Core;
+using EverydayPatientInfo.MVVM.ViewModel;
+
+namespace EverydayPatientInfo.MVVM.Model
+{
+    class PasswordResetModel
+    {
+        private PasswordResetViewModel passwordResetVM;
+
+        public bool ResetPassword(string CardID, string Password)
+        {
+            if (false) // if person with card id was found in database
+                return true;
+            return false;
+        }
+
+        public PasswordResetModel(PasswordResetViewModel passwordResetVM)
+        {
+            this.passwordResetVM = passwordResetVM;
+        }
+    }
+}
--- EverydayPatientInfo/EverydayPatientInfo/MVVM/ViewModel/PasswordResetViewModel.cs
+++ EverydayPatientInfo/EverydayPatientInfo/MVVM/ViewModel/PasswordResetViewModel.cs
@@ -6,18 +6,57 @@ namespace EverydayPatientInfo.MVVM.ViewModel
 {
     class PasswordResetViewModel : ObservableObject
     {
+        #region Private fields
+
+        private PasswordResetModel passwordResetModel;
+
+        #endregion
+
+        #region Public properties
         public string CardID { get; set; }
         public string Password1 { get; set; }
         public string Password2 { get; set; }
 
-        public ICommand ResetPassword { get; set; }
-        public ICommand ToLoginPage { get; set; }
+        public ICommand ResetPasswordCommand { get; set; }
+        public ICommand LoginCommand { get; set; }
+        #endregion
 
+        #region Constructor
         public PasswordResetViewModel()
         {
+            passwordResetModel = new(this);
 
+            ResetPasswordCommand = new RelayCommand(Reset);
+            LoginCommand = new RelayCommand(ToLoginPage);
+        }
+        #endregion
+
+        #region Private methods
+
+        private void Reset()
+        {
+            if (Password1 != Password2)
+                Clear();
+            else if (passwordResetModel.ResetPassword(CardID, Password1))
+                ToLoginPage();
+            else
+                Clear();
+                    
         }
 
-        
+        private void Clear()
+        {
+            Password1 = "";
+            Password2 = "";
+        }
+
+        private void ToLoginPage()
+        {
+            MainWindowViewModel.Instance.CurrentView = MainWindowViewModel.Instance.SignInVM;
+        }
+        #endregion
+
+
+
     }
 }
